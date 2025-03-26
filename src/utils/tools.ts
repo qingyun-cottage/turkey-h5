c6d8c84254370d3ceb90d75dbffbce65f0065ead
niyang <niyang@sundear.com>
2025-03-26 14:34:21 +0800

    更新 tools.ts

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,12 +1,3 @@
-// 为了解决找不到模块的问题，需要安装 weixin-js-sdk 及其类型声明
-// 如果项目使用 npm，可以运行以下命令：npm install weixin-js-sdk @types/weixin-js-sdk
-// 如果项目使用 yarn，可以运行以下命令：yarn add weixin-js-sdk @types/weixin-js-sdk
-// 为了解决声明未使用的问题，这里暂时添加一个简单的使用示例
-// import wx from 'weixin-js-sdk';
-
-// 这里可以添加一些实际使用 wx 的代码，例如简单的日志输出
-// console.log('weixin-js-sdk 已导入:', wx);
-
 export const initHome = () => {
     console.log('initHome,下一步')
 }
